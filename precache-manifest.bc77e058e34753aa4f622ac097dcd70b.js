ed42fbd9c46c2023193a61334280a52381e04981
cameron9 <cameronhk11@yahoo.com>
2020-10-04 20:25:21 -0700

    Updates

---
+++ precache-manifest.bc77e058e34753aa4f622ac097dcd70b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cbd1ba0ac13f41c3e6c1530b6c333e",
+    "url": "/Superstar8.github.io/coin-exchange2,/index.html"
+  },
+  {
+    "revision": "66e6a0be0c87a6a12dec",
+    "url": "/Superstar8.github.io/coin-exchange2,/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b37ae45422c1df32efc2",
+    "url": "/Superstar8.github.io/coin-exchange2,/static/js/2.d2ae1bb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Superstar8.github.io/coin-exchange2,/static/js/2.d2ae1bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e6a0be0c87a6a12dec",
+    "url": "/Superstar8.github.io/coin-exchange2,/static/js/main.62035dc2.chunk.js"
+  },
+  {
+    "revision": "c562f3dc8c9a05b29740",
+    "url": "/Superstar8.github.io/coin-exchange2,/static/js/runtime-main.580de820.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Superstar8.github.io/coin-exchange2,/static/media/logo.5d5d9eef.svg"
+  }
+]);
